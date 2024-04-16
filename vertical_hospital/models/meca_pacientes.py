3e81268ca9d8290b9b186f686b461be22f24f7f8
Jonathan Lorenzo <141760654+jonalorenzo@users.noreply.github.com>
2024-04-15 20:32:02 -0600

    Add files via upload

--- vertical_hospital/models/meca_pacientes.py
+++ vertical_hospital/models/meca_pacientes.py
@@ -9,9 +9,9 @@ class MecaPacientes(models.Model):
     name = fields.Char('Nombre y apellido', required=True, tracking=True)
     sequence = fields.Char(string='Secuencia', readonly=True, required=True, copy=False, default=lambda self: _('New'))
     dni = fields.Char('DNI', required=True, tracking=True)
-    treatment_ids = fields.Many2many('meca.tratamientos', string='Tratamientos', options="{'no_create': True}")
+    treatment_ids = fields.Many2many('meca.tratamientos', string='Tratamientos')
     admission_date = fields.Datetime('Fecha de alta', default=fields.Datetime.now)
-    update_date = fields.Datetime('Hora de actualización', readonly=True)
+    update_date = fields.Datetime('Hora de actualización')
     state = fields.Selection([
         ('draft', 'Borrador'),
         ('active', 'Alta'),
