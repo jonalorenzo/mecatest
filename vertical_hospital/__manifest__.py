eee96c373c4aa3b5d666e4b7b102f3194123292c
Jonathan Lorenzo <141760654+jonalorenzo@users.noreply.github.com>
2024-04-15 18:22:53 -0600

    Add files via upload

---
+++ vertical_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vertical Hospital',
+    'version': '16.0.0.1',
+    'category': 'Uncategorized',
+    'summary': 'Vertical Hospital',
+    'license':'AGPL-3',
+    'description': """
+        Vertical Hospital
+""",
+    'author' : 'Jonathan Lorenzo',
+    'depends': [''],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
